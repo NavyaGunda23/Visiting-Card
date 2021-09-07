a187a98aab77a952846ddac41c1c9fd7339fe2b6
NavyaGunda23 <52776081+NavyaGunda23@users.noreply.github.com>
2021-09-07 18:07:09 +0530

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [ useDetails, setUserDetails] = useState([])
+
+  useEffect(()=>{
+  getData()
+
+  })
+
+  async function getData(){
+    let request =  await axios.get('https://jsonplaceholder.typicode.com/users');
+    setUserDetails(useDetails => request.data)
+    console.log(request)
+    return request.data
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="card_layout">
+            {useDetails.map(x => (
+                    <div className="card"> 
+                    <p className="mt-0">{x.name}</p>
+                      <p><i className = "icon16 di-block icon-email v-middle mr-10"></i><span className="info-text">{x.email}</span></p>
+                      <p><i className = "icon16 di-block icon-phone v-middle mr-10"></i><span className="info-text">{x.phone}</span></p>
+                      <p className="d-flex mb-0"> <i className = "icon20 di-block icon-location v-middle mr-10"></i><span className="info-text">{x.address.street},{x.address.city},{x.address.zipcode},{x.address.suite}</span></p>
+                    
+                    </div>
+            ))}
+      </div>
     </div>
   );
 }
